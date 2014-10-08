540f677755fca944af64d465ce5e87fb07e70f99
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-08 18:25:21 +0530

    implementing the alto

--- MinimumCoinChange/MinimumCoinChange/main.c
+++ MinimumCoinChange/MinimumCoinChange/main.c
@@ -9,25 +9,44 @@
 #include <stdio.h>
 #include <string.h>
 
+int findMinimumCoinChange(int rs, const int coinSetPtr [], size_t coinSetLength);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    int amount = 0;
+    int minimumCoinChange = 0;
+    int coinValue[3] = {1, 3, 5};
+    printf("Enter the amount in rs:");
+    scanf("%d", &amount);
+    minimumCoinChange = findMinimumCoinChange(amount, coinValue, sizeof(coinValue)/sizeof(coinValue[0]));
+    printf("Minimum Coin require is:%d", minimumCoinChange);
     return 0;
 }
 
-int findMinimumCoinChange(int rs, const int (*coinSetPtr) []){
-    int coinSetLength = sizeof(coinSetPtr) / sizeof(int);
+int findMinimumCoinChange(int rs, const int coinSetPtr [], size_t coinSetLength){
     int minCoinValues[rs+1];
+    int finalSolutiuon = 0;
     memset(minCoinValues, -1, rs+1);
     minCoinValues[0] = 0;
     for(int i = 1; i <= rs ; i++){
         int solution[coinSetLength];
         memset(solution, -1, coinSetLength);
         for (int j=0; j < coinSetLength; j++) {
-            if((*coinSetPtr)[j] <= i){
-                solution[j] = minCoinValues[i-(*coinSetPtr)[j]] + 1;
+            if(coinSetPtr [j] <= i){
+                solution[j] = minCoinValues[i - coinSetPtr[j]] + 1;
             }
         }
+        
+        finalSolutiuon = -1;
+        for(int k=0;k < coinSetLength;k++){
+            if(solution[k] > 0 ){
+                if(finalSolutiuon == -1 || solution[k] < finalSolutiuon){
+                    finalSolutiuon = solution[k];
+                }
+            }
+        }
+        
+        
     }
     return minCoinValues[rs];
 }
