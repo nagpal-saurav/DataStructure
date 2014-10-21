4f2e01329e7565f4283e9d5227e0e7f569a60290
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-21 16:47:27 +0530

    Implemented the maximum sub sequence problem

--- MaximumSubSequenceSum/MaximumSubSequenceSum/main.c
+++ MaximumSubSequenceSum/MaximumSubSequenceSum/main.c
@@ -6,17 +6,17 @@
 //  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
 //
 
+#include "string.h"
 #include <stdio.h>
 
 int calculateMaximumSubSequence(int array[], int size);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
     // insert code here...
     int max_sub_sequence = 0;
     //int price[4] = {1, 5, 8, 9};
-    int array[8] = {1, 5, 8, 9, 10, 17, 17, 20};
+    int array[8] = {-2, -3, 4, -1, -2, 1, 5, -3};
     max_sub_sequence = calculateMaximumSubSequence(array, sizeof(array)/sizeof(array[0]));
     printf("Minimum Coin require is:%d", max_sub_sequence);
     return 0;
@@ -24,6 +24,18 @@ int main(int argc, const char * argv[]) {
 
 int calculateMaximumSubSequence(int array[], int size){
     int max[size];
-    
-    return max[size-1];
+    memset(max, 0, size);
+    int maximumSubsequence = 0;
+    max[0] = array[0];
+    for(int i = 1; i < size;i++){
+        if(max[i - 1] > 0){
+            max[i] = max[i-1]+array[i];
+        }else{
+            max[i] = array[i];
+        }
+        if(maximumSubsequence < max[i]){
+            maximumSubsequence = max[i];
+        }
+    }
+    return maximumSubsequence;
 }
