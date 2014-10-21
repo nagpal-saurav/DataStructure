a76b8d11b6c3cc368869855a1a2b059ff51e4849
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-21 16:12:26 +0530

    Start implementing the solution

--- MaximumSubSequenceSum/MaximumSubSequenceSum/main.c
+++ MaximumSubSequenceSum/MaximumSubSequenceSum/main.c
@@ -8,8 +8,22 @@
 
 #include <stdio.h>
 
+int calculateMaximumSubSequence(int array[], int size);
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
+    // insert code here...
+    int max_sub_sequence = 0;
+    //int price[4] = {1, 5, 8, 9};
+    int array[8] = {1, 5, 8, 9, 10, 17, 17, 20};
+    max_sub_sequence = calculateMaximumSubSequence(array, sizeof(array)/sizeof(array[0]));
+    printf("Minimum Coin require is:%d", max_sub_sequence);
     return 0;
+}
+
+int calculateMaximumSubSequence(int array[], int size){
+    int max[size];
+    
+    return max[size-1];
 }
