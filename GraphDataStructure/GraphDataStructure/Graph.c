62f2558a7e8d08a5fae84b19362ca418bb31a019
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-27 18:33:53 +0530

    Implementing the graph

--- GraphDataStructure/GraphDataStructure/Graph.c
+++ GraphDataStructure/GraphDataStructure/Graph.c
@@ -6,4 +6,58 @@
 //  Copyright (c) 2014 Saurav Nagpal. All rights reserved.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
 #include "Graph.h"
+
+#define  matrix_count       2u
+
+struct graph_internal_struct{
+    graph_storage_type_e  type;
+    graph_vertex_count    vertexCount;
+    SNGraphVertex         *vertices;
+    int                   *graph;
+};
+
+
+
+typedef struct graph_matrix_struct{
+    int     **graph_matrix;
+}graph_matrix;
+
+
+
+SNGraph* newGraph(graph_storage_type_e type, graph_vertex_count count){
+    SNGraph* newGraph = (SNGraph*) malloc(sizeof(SNGraph));
+    newGraph->graph_data = (graph_internal*) malloc(sizeof(graph_internal));
+    newGraph->graph_data->type = type;
+    newGraph->vertexCount = count;
+    newGraph->graph_data->vertices = malloc(sizeof(SNGraphVertex) * count);
+    switch (type) {
+        case graph_storage_adjacency_matrix:{
+                newGraph->graph_data->graph = malloc(sizeof(int) * count * matrix_count);
+            }
+            break;
+        case graph_storage_adjacency_list:{
+            
+            break;
+        }
+        default:
+            break;
+    }
+    
+    return newGraph;
+}
+
+bool addVertex(SNGraph *graph, const char* name){
+    SNGraphVertex* vertex = (SNGraphVertex*)malloc(sizeof(SNGraphVertex));
+    vertex->vertexName = malloc(strlen(name));
+    strcpy(vertex->vertexName, name);
+    
+    return true;
+}
+
+bool addEdge(SNGraph *graph, SNGraphVertex *vertex1, SNGraphVertex *vertex2){
+    return true;
+}
--- GraphDataStructure/GraphDataStructure/Graph.h
+++ GraphDataStructure/GraphDataStructure/Graph.h
@@ -9,6 +9,32 @@
 #ifndef __GraphDataStructure__Graph__
 #define __GraphDataStructure__Graph__
 
-#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct graph_struct SNGraph;
+typedef struct graph_vertex_struct SNGraphVertex;
+typedef struct graph_vertex_edge   SNGraphEdge;
+typedef int    graph_vertex_count;
+
+typedef struct graph_internal_struct graph_internal;
+
+struct graph_struct{
+    graph_vertex_count    vertexCount;
+    graph_internal        *graph_data;
+};
+
+struct graph_vertex_struct{
+    char*      vertexName;
+};
+
+typedef enum {
+   graph_storage_adjacency_matrix,
+   graph_storage_adjacency_list
+}graph_storage_type_e;
+
+
+SNGraph* newGraph(graph_storage_type_e type, graph_vertex_count count);
+bool addVertex(SNGraph *graph, const char* name);
+bool addEdge(SNGraph *graph, SNGraphVertex *vertex1, SNGraphVertex *vertex2);
 
 #endif /* defined(__GraphDataStructure__Graph__) */
