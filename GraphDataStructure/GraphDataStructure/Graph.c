5c3e6da634498a2a67041f35448dc6c16f28446b
nagpal-saurav <nagpalsaurav@gmail.com>
2014-10-28 12:15:21 +0530

    Implement the graph for adjacencyList

--- GraphDataStructure/GraphDataStructure/Graph.c
+++ GraphDataStructure/GraphDataStructure/Graph.c
@@ -14,6 +14,9 @@
 #define  matrix_count       2u
 #define  Invalid_Index      -1
 
+typedef struct graph_adjacency_list_struct adjacency_list;
+typedef struct graph_adjacency_head_struct adjacency_head;
+
 struct graph_internal_struct{
     graph_storage_type_e  type;
     graph_vertex_count    currentCount;
@@ -21,12 +24,24 @@ struct graph_internal_struct{
     int                   *graph;
 };
 
-typedef struct graph_matrix_struct{
+typedef struct adjacency_matrix{
     int     **graph_matrix;
 }graph_matrix;
 
+struct graph_adjacency_list_struct{
+    SNGraphVertex       *toVertex;
+    void*               data;
+    adjacency_list      *next_vertex;
+};
+
+struct graph_adjacency_head_struct{
+    int                 degreeOfvertex;
+    adjacency_list      *headVertex;
+};
+
 bool addVertexToGraphVertices(SNGraph *graph, SNGraphVertex* vertex);
 int findVertexIndex(SNGraph* graph, SNGraphVertex* vertex);
+adjacency_list* newAdjacencyEdge(SNGraphVertex* vertex);
 
 SNGraph* newGraph(graph_storage_type_e type, graph_vertex_count count){
     SNGraph* newGraph = (SNGraph*) malloc(sizeof(SNGraph));
@@ -40,11 +55,11 @@ SNGraph* newGraph(graph_storage_type_e type, graph_vertex_count count){
     newGraph->graph_data->vertices = (SNGraphVertex**)malloc(sizeof(SNGraphVertex*) * count);
     switch (type) {
         case graph_storage_adjacency_matrix:{
-                newGraph->graph_data->graph = malloc(sizeof(int*) * count * matrix_count);
-            }
+            newGraph->graph_data->graph = malloc(sizeof(int*) * count * matrix_count);
             break;
+        }
         case graph_storage_adjacency_list:{
-            
+            newGraph->graph_data->graph  = malloc(sizeof(adjacency_head) * count);
             break;
         }
         default:
@@ -66,16 +81,37 @@ SNGraphVertex* addVertex(SNGraph *graph, const char* name){
     return NULL;
 }
 
-SNGraphEdge* addEdge(SNGraph *graph, SNGraphVertex *vertex1, SNGraphVertex *vertex2, int weight){
+SNGraphEdge* addEdge(SNGraph *graph, SNGraphVertex *vertex1, SNGraphVertex *vertex2, void* data){
     SNGraphEdge* edge = (SNGraphEdge*)malloc(sizeof(SNGraphEdge));
     if(vertex1 == vertex2){
         return NULL;
     }
-    int index1 = findVertexIndex(graph, vertex1);
-    int index2 = findVertexIndex(graph, vertex2);
-    if(index1 != Invalid_Index && index2 != Invalid_Index){
-        int** graphBuf = (int**)graph->graph_data->graph;
-        graphBuf[index1][index2] = graphBuf[index2][index1] = weight;
+    switch (graph->graph_data->type) {
+        case graph_storage_adjacency_matrix:{
+            int index1 = findVertexIndex(graph, vertex1);
+            int index2 = findVertexIndex(graph, vertex2);
+            if(index1 != Invalid_Index && index2 != Invalid_Index){
+                int** graphBuf = (int**)graph->graph_data->graph;
+                graphBuf[index1][index2] = graphBuf[index2][index1] = *(int*)data;
+            }
+            break;
+        }
+        case graph_storage_adjacency_list:{
+            int index1 = findVertexIndex(graph, vertex1);
+            if(index1 != Invalid_Index){
+                adjacency_head* graphBuf =(adjacency_head*) graph->graph_data->graph;
+                adjacency_list* newEdge = newAdjacencyEdge(vertex2);
+                newEdge->data = data;
+                if(graphBuf->degreeOfvertex == 0){
+                    graphBuf->headVertex = newEdge;
+                }else{
+                    newEdge->next_vertex = graphBuf->headVertex->next_vertex;
+                    graphBuf->headVertex->next_vertex = newEdge->next_vertex;
+                }
+            }
+            break;
+        }
+
     }
     return edge;
     
@@ -103,4 +139,11 @@ int findVertexIndex(SNGraph* graph, SNGraphVertex* vertex){
         }
     }
     return index;
+}
+
+adjacency_list* newAdjacencyEdge(SNGraphVertex* vertex){
+    adjacency_list* newEdge = (adjacency_list*)malloc(sizeof(adjacency_list));
+    newEdge->toVertex = vertex;
+    newEdge->next_vertex = NULL;
+    return newEdge;
 }
